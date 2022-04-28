50357a7c341859ee80c74c9b21bf4febd43294e0
arahito1994 <artht0408@gmail.com>
2022-04-29 07:08:13 +0900

    18

---
+++ familyProf.java/src/AssignmentOperator.java
@@ -0,0 +1,27 @@
+import java.lang.invoke.StringConcatFactory;
+
+public class AssignmentOperator {
+  public static void main(String[] args) {
+    // 1. 変数宣言、変数への代入処理:
+    // 変数「a」に 「4」を代入する
+    int a = 4;
+    // 変数「target」を宣言する
+    int target;
+    // 2. 値の代入に「=」を使う:
+    // 変数「target」への変数「a」の代入に「=」を使う
+    target = a;
+    // 変数「target」を表示する
+    System.out.printf("target = a: %d%n", target);
+    // 3. 値の代入に「+=」を使う:
+    // 変数「target」への変数「a」の代入に「+=」を使う
+    target += a;
+    // 変数「target」を表示する
+    System.out.printf("target += a: %d%n", target);
+    // 4. 値の代入に「*=」を使う:
+    // 変数「target」への変数「a」の代入に「*=」を使う
+    target *= a;
+    // 変数「target」を表示する
+    System.out.printf("target *= a: %d%n", target);
+
+  }
+}
