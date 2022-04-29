04a52e038dc8814a7b6be4e33dbecb2f5e457ec6
arahito1994 <artht0408@gmail.com>
2022-04-29 18:33:40 +0900

    23_05

--- familyProf.java/src/PercentFormatter.java
+++ familyProf.java/src/PercentFormatter.java
@@ -1,5 +1,10 @@
+import java.text.DecimalFormat;
+
 public class PercentFormatter {
   public static void main(String[] args) {
-    
+    double AnnualRate = 123456.78935465;
+
+    DecimalFormat precentFormat = new DecimalFormat("0,000.##%");
+    System.out.println(precentFormat.format(AnnualRate));
   }
 }
