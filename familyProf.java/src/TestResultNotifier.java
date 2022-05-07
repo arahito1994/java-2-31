a4f1a652418d4f2acf0f24eb00a22d9bf5f08a9b
arahito1994 <artht0408@gmail.com>
2022-05-07 18:12:56 +0900

    リファクタリング

--- familyProf.java/src/TestResultNotifier.java
+++ familyProf.java/src/TestResultNotifier.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TestResultNotifier {
   public static void main(String[] args) {
 
-
+    final int PASS_SCORE_MIN = 70;
     System.out.println("100点満点中、何点だったか回答してください。");
     // 数学の得点は？を表示
     System.out.print("数学の得点は？: ");
@@ -14,10 +14,9 @@ public class TestResultNotifier {
     System.out.println(mathScore);
 
     // 三項演算子を使用して、70点以上なら合格、69点以下なら不合格を判定する
-    String result = mathScore >= 70 ? "合格" : "不合格";
+    String result = (mathScore >= PASS_SCORE_MIN) ? "合格" : "不合格";
 
     System.out.printf("数学のテストは %s です。 %n", result);
-
     score.close();
 
   }
