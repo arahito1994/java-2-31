19d304f42e60f2015db31e1ec5c6f934ab6a60b9
arahito1994 <artht0408@gmail.com>
2022-05-07 19:04:27 +0900

    32_1

---
+++ familyProf.java/src/TextSpecificTimesPrinter.java
@@ -0,0 +1,7 @@
+public class TextSpecificTimesPrinter {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 12; i++){
+      System.out.printf("%d: Java は簡単で楽しい。%n", i);
+    }
+  }
+}
