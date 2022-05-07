36aef6ce0a27aaa6f6185750db419a92ad291b78
arahito1994 <artht0408@gmail.com>
2022-05-07 19:14:27 +0900

    31_2

---
+++ familyProf.java/src/ReverseOrderNumberPrinter.java
@@ -0,0 +1,8 @@
+public class ReverseOrderNumberPrinter {
+  public static void main(String[] args) {
+    final int TIMES = 12;
+    for (int i = TIMES; i >= 1; i--) {
+      System.out.printf("%2d %n" , i);
+    }
+  }
+}
