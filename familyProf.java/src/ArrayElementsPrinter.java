920c00c44ee36d8f50bd981994e93181f3fc3b61
arahito1994 <artht0408@gmail.com>
2022-05-08 13:55:05 +0900

    31_3

---
+++ familyProf.java/src/ArrayElementsPrinter.java
@@ -0,0 +1,8 @@
+public class ArrayElementsPrinter {
+  public static void main(String[] args) {
+    String dogTypes[] = { "Chiwawa", "Pomeranian", "Bulludog", "Akita", "Poodle" };
+    for (int i = 0; i <= dogTypes.length; i++) {
+      System.out.printf("dogTypes の %d 番目の犬種は %s です。 %n", i + 1, dogTypes[i]);
+    }
+  }
+}
